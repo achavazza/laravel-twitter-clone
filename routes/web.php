f02cf161541ad10e5412b6c8c1eb1eb048e93958
Alejandro Chavazza <alejandrochavazza@gmail.com>
2025-03-04 14:38:35 +0100

    timeline controller

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/api/timeline', [App\Http\Controllers\Api\TimelineController::class, 'index']);
 /*
 Auth::routes();
 
